1f3e46051520a0f593c1fb2a14110fca038edcc7
Fabian <ornelasf1@gmail.com>
2023-02-26 02:23:01 -0600

    added tailwindcss

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Start dev environment
+*Note: Because Foreman is broken, we have to use 2 cmd prompts. Foreman is used to start multiple services at once. Normally Foreman is ran by running the script `bin/dev`*
 
+In one prompt for Tailwind, run the following to watch for css changes.
+```
+bin/rails tailwindcss:watch
+or
+rake tailwindcss:watch
+```
+Start the Ruby on Rails server by running. Default port is 3000
+```
+bin/rails server
+```
+### Tailwind CSS
+We must build our Tailwind styling into a single tailwind file to make sure we only bring in classes that are necessary.
+We run the following to build the file.
+```
+rake tailwindcss:build
+```
+The built tailwind css file is `public/css/style.css`
+
+### Notes
 Things you may want to cover:
 
 * Ruby version
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,12 +5,35 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        inter: ['Inter', 'sans-serif'],
+        'playfair-display': ['Playfair Display', 'serif'],
+        'pacifico': ['Pacifico'],
+      },
+      fontSize: {
+        xs: ['0.75rem', { lineHeight: '1.5' }],
+        sm: ['0.875rem', { lineHeight: '1.5715' }],
+        base: ['1rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
+        lg: ['1.125rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
+        xl: ['1.25rem', { lineHeight: '1.5', letterSpacing: '-0.01em' }],
+        '2xl': ['1.5rem', { lineHeight: '1.415', letterSpacing: '-0.01em' }],
+        '3xl': ['2rem', { lineHeight: '1.3125', letterSpacing: '-0.01em' }],
+        '4xl': ['2.25rem', { lineHeight: '1.25', letterSpacing: '-0.01em' }],
+        '5xl': ['3rem', { lineHeight: '1.2', letterSpacing: '-0.01em' }],
+        '6xl': ['4rem', { lineHeight: '1.1562', letterSpacing: '-0.01em' }],
+      },
+      letterSpacing: {
+        tighter: '-0.02em',
+        tight: '-0.01em',
+        normal: '0',
+        wide: '0.01em',
+        wider: '0.02em',
+        widest: '0.4em',
       },
     },
   },
