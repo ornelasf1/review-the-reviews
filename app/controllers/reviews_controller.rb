7d9a41b2ea7a9ec60cb68ec8b8dda443760f75cd
Fabian <ornelasf1@gmail.com>
2023-03-02 21:11:42 -0600

    cleaned up submit form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def create
     @reviewer = Reviewer.find(params[:reviewer_id])
     @review = @reviewer.reviews.create(review_params)
-    @rating = Rating.create(wellwritten: params[:wellwritten], useful: params[:useful], usability: params[:usability], entertainment: params[:entertainment])
+    @rating = Rating.create(rating_params)
     @review.rating = @rating
     redirect_to reviewer_path(@reviewer)
   end
@@ -11,5 +11,8 @@ class ReviewsController < ApplicationController
   private
   def review_params
     params.require(:review).permit(:commenter, :body)
+  end
+  def rating_params
+    params.permit(:wellwritten, :useful, :usability, :entertainment)
   end
 end
