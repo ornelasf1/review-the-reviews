a98a603b29fdbd8f8d7579c85d19aa8b43e3db70
Fabian <ornelasf1@gmail.com>
2023-03-05 12:33:50 -0600

    Fixed infinity rview

--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -7,31 +7,32 @@ class Reviewer < ApplicationRecord
 
     validates :name, presence: true, length: {minimum:3, maximum: 100}
     validates :review, presence: true, length: {minimum:20, maximum: 1000}
-    # Not working for some reason?!
     validates :platform, presence: true, inclusion: { in: $platformTitleMap.keys.map { |sym| sym.to_s } }
     validates :category, presence: true, inclusion: { in: $categoryTitleMap.keys.map { |sym| sym.to_s } }
 
     def finalRating
+        require 'json'
         if self.reviews.count == 0
           return 0
         end
         finalTotal = self.reviews.reject{ |review| review.rating == nil }.reduce(0) do |aggregate, review|
             total = 0
             numOfRatings = 0
+            puts review.rating.to_json
             unless review.rating.wellwritten.blank?
-                total += review.rating.wellwritten
+                total += review.rating.wellwritten.to_i
                 numOfRatings += 1
             end
             unless review.rating.usability.blank?
-                total += review.rating.usability
+                total += review.rating.usability.to_i
                 numOfRatings += 1
             end
             unless review.rating.entertainment.blank?
-                total += review.rating.entertainment
+                total += review.rating.entertainment.to_i
                 numOfRatings += 1
             end
             unless review.rating.useful.blank?
-                total += review.rating.useful
+                total += review.rating.useful.to_i
                 numOfRatings += 1
             end
 
@@ -39,7 +40,7 @@ class Reviewer < ApplicationRecord
 
             aggregate += total
         end
-        finalRating = finalTotal / self.reviews.reject {|review| review.rating != nil}.count
+        finalRating = finalTotal / self.reviews.reject {|review| review.rating.blank? }.count
         return finalRating
     end
 
