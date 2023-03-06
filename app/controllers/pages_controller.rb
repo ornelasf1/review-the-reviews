b146bbad719e5804908090f2f09450787798fd39
Fabian <ornelasf1@gmail.com>
2023-03-06 14:36:53 -0600

    Add search helper modulea and canvased search page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
   def index
     @recent_reviewers = Reviewer.order(:created_at).reverse_order.limit(5)
   end
+
+  def search
+    relavent_reviewers = get_reviewers_for_product 'god of war 4'
+    @reviewers = Reviewer.find(relavent_reviewers)
+  end
+
+  def get_reviewers_for_product
+    reviewers = Reviewer.where(category: params[:category])
+    # search google using reviewers
+  end
 end
--- lib/search_api.rb
+++ lib/search_api.rb
@@ -1,4 +1,4 @@
-class SearchApi
+module SearchApi
     # require 'nokogiri'
     require 'open-uri'
     require 'json'
@@ -10,13 +10,40 @@ class SearchApi
     #     puts item['href']
     # end
 
-    print 'Category to browse: '
-    category = gets.chomp
-    api_key='AIzaSyDEB47WaNPX9CUha57jp8CpupSkhPVguRk'
-    query="#{category} reviews"
-    response = URI.open("https://www.googleapis.com/customsearch/v1?cx=77365176e0c1142f0&key=#{api_key}&q=#{query}")
-    json = JSON.parse(response.read)
-    json['items'].each do |obj|
-        puts obj['link']
+    # print 'Category to browse: '
+    # category = gets.chomp
+    # api_key='AIzaSyDEB47WaNPX9CUha57jp8CpupSkhPVguRk'
+    # query="#{category} reviews"
+    # response = URI.open("https://www.googleapis.com/customsearch/v1?cx=77365176e0c1142f0&key=#{api_key}&q=#{query}")
+    # json = JSON.parse(response.read)
+    # json['items'].each do |obj|
+    #     puts obj['link']
+    # end
+    API_KEY=''
+
+    def search_reviews websites, query
+        sites_query = websites.map{ |site| "site:#{site}"}.join ' OR '
+        query="#{sites_query.blank? ? '' : sites_query + ' '}#{query} reviews"
+        begin
+            response = URI.open("https://www.googleapis.com/customsearch/v1?cx=77365176e0c1142f0&key=#{API_KEY}&q=#{query}")
+            puts "Searching https://www.googleapis.com/customsearch/v1?cx=77365176e0c1142f0&key=xxx&q=#{query}"
+            json = JSON.parse(response.read)
+
+            hostname_to_reviews = Hash.new
+            json['items'].map do |item|
+                hostname = URI.parse(item['link']).host
+                 if hostname_to_reviews.has_key?(hostname)
+                    hostname_to_reviews[hostname].push(item['link'])
+                 else
+                    hostname_to_reviews[hostname] = [item['link']]
+                 end
+            end
+        rescue => e
+            p "error parsing #{json}"
+            p e.message
+            return Hash.new
+        else
+            return hostname_to_reviews
+        end
     end
 end
