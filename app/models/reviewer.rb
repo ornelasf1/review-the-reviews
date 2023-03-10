321428862643d0efd88b73abe8fe856fec6edb4c
Fabian <ornelasf1@gmail.com>
2023-03-09 20:54:42 -0600

    add streamer improveents

--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -16,7 +16,7 @@ class Reviewer < ApplicationRecord
 
     validates :name, presence: true, length: {minimum:3, maximum: 100}
     validates :review, presence: true, length: {minimum:20, maximum: 1000}
-    validates :hostname, presence: true, format: { with: /\A(?:https?:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)\z/, message: "invalid" }
+    validates :hostname, presence: true, if: -> { platform == 'website' } , format: { with: /\A(?:https?:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)\z/, message: "invalid" }
     validates :platform, presence: true, inclusion: { in: $platformTitleMap.keys.map { |sym| sym.to_s } }
 
     def finalRating
