fea3be046ae6d0e1480c252bbe9db402ff0dce80
Fabian <ornelasf1@gmail.com>
2023-03-15 23:27:55 -0500

    Updated turbo frame tag to re-render all reviews on update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,23 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @reviewer = Reviewer.find(params[:reviewer_id])
+    @review = @reviewer.reviews.new
+  end
+
   def create
     @reviewer = Reviewer.find(params[:reviewer_id])
     if user_signed_in?
       @review = @reviewer.reviews.new(review_params)
       @review.user_id = current_user.id
-      @review.save
-  
-      redirect_to reviewer_path(@reviewer)
+      if @review.save
+        respond_to do |format|
+          format.html { redirect_to reviewer_path(@reviewer), notice: 'Review was successfully created.' }
+          format.turbo_stream
+        end
+      else
+        render :new, status: :unprocessable_entity
+      end
     else
       redirect_to reviewer_path(@reviewer), status: :unauthorized
     end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,11 +1,11 @@
 class Rating < ApplicationRecord
   belongs_to :review
 
-  validate :any_rating_present?
+  # validate :any_rating_present?
 
   def any_rating_present?
     if wellwritten.blank? and usability.blank? and useful.blank? and entertainment.blank? and quality.blank? and insightful.blank?
-      errors.add :base, "One of these ratings have to be set"
+      errors.add(:base, :a_rating_empty, message: "One of these ratings have to be set")
     end
   end
 end
