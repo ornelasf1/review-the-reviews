368fe3e8f3e9b5b8aaef6a929820ceb5a81f56d9
Fabian <ornelasf1@gmail.com>
2023-03-17 01:20:40 -0500

    validate rating obj and prevent reviews with no review or rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,8 +8,12 @@ class Review < ApplicationRecord
   belongs_to :user
   has_one :rating, dependent: :destroy
 
-  accepts_nested_attributes_for :rating
+  accepts_nested_attributes_for :rating, :reject_if => :rating_attrs_blank? # Prevents a rating object to be created so we can do review.rating.blank? check instead of checking all attributes
 
   validates :commenter, length: {minimum:3, maximum:30}
-  validates :body, length: {maximum: 2000}
+  validates :body, length: {maximum: 2000}, :presence => true, if: -> {rating.blank?}
+
+  def rating_attrs_blank? attrs
+    %i(usability wellwritten entertainment useful insightful quality).all? {|sym| attrs[sym].blank? }
+  end
 end
