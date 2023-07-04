32998955058529a253a7d271d7334409f32580f9
Fabian <ornelasf1@gmail.com>
2023-07-04 14:40:30 -0500

    add test for super mario bros movie

--- test/unit/product_search_test.rb
+++ test/unit/product_search_test.rb
@@ -6,13 +6,168 @@ class ProductSearchTest < ActiveSupport::TestCase
         @object.extend(ProductSearch)
     end
 
-    test "product search the truth" do
+    test "product search rottentomatoes dune" do
         hostname_to_urls = {"www.rottentomatoes.com" => [
             "https://www.rottentomatoes.com/m/dune_2021", 
             "https://www.rottentomatoes.com/m/1006364-dune", 
             "https://www.rottentomatoes.com/m/10010089-dune", 
             "https://www.rottentomatoes.com/m/dune_2021/reviews"]
         }
+        products_map = @object.get_products(hostname_to_urls, "movies")
+        expected_product = Product.new(name: "Dune", score: 83, maxscore: 100, source: "https://www.rottentomatoes.com/m/dune_2021", initial_source: "https://www.rottentomatoes.com/m/dune_2021")
+        assert_equal(expected_product, products_map["www.rottentomatoes.com"], "scraped product did not match")
+    end
+
+    test "product search category 'movies' query 'super mario bros'" do
+        hostname_to_urls = {
+            "www.ign.com": [
+              "https://www.ign.com/articles/the-super-mario-bros-movie-review",
+              "https://www.ign.com/movies/the-super-mario-bros-movie",
+              "https://www.ign.com/articles/super-mario-bros-movie-chris-pratt-says-to-get-ready-for-a-lot-of-these-movies"
+            ],
+            "www.metacritic.com/movie": [
+              "https://www.metacritic.com/movie/the-super-mario-bros-movie",
+              "https://www.metacritic.com/movie/the-super-mario-bros-movie/critic-reviews",
+              "https://www.metacritic.com/movie/super-mario-bros"
+            ],
+            "www.rogerebert.com": [
+              "https://www.rogerebert.com/reviews/the-super-mario-bros-movie-movie-review-2023",
+              "https://www.rogerebert.com/reviews/high-score-2020",
+              "https://www.rogerebert.com/reviews/8-bit-christmas-movie-review-2021"
+            ],
+            "www.rottentomatoes.com": [
+              "https://www.rottentomatoes.com/m/the_super_mario_bros_movie",
+              "https://www.rottentomatoes.com/m/super_mario_bros",
+              "https://www.rottentomatoes.com/"
+            ],
+            "www.imdb.com": [
+              "https://www.imdb.com/title/tt6718170/",
+              "https://www.imdb.com/title/tt6718170/reviews/",
+              "https://www.imdb.com/news/ni64029292/"
+            ],
+            "www.commonsensemedia.org": [
+              "https://www.commonsensemedia.org/movie-reviews/the-super-mario-bros-movie",
+              "https://www.commonsensemedia.org/movie-reviews/the-super-mario-bros-movie/user-reviews/adult",
+              "https://www.commonsensemedia.org/game-reviews/new-super-mario-bros-u-deluxe",
+              "https://www.commonsensemedia.org/game-reviews/new-super-mario-bros-u",
+              "https://www.commonsensemedia.org/game-reviews/new-super-mario-bros-wii",
+              "https://www.commonsensemedia.org/game-reviews/new-super-mario-bros",
+              "https://www.commonsensemedia.org/game-reviews/new-super-mario-bros-2"
+            ],
+            "ew.com": [
+              "https://ew.com/movies/super-mario-bros-tops-box-office/",
+              "https://ew.com/movies/movie-reviews/the-super-mario-bros-movie-review/",
+              "https://ew.com/article/2012/11/20/super-mario-batman-wii-u-games/",
+              "https://ew.com/creative-work/super-mario-bros-movie/",
+              "https://ew.com/gallery/15-videogame-based-movies-howd-they-score/",
+              "https://ew.com/genre/animated/",
+              "https://ew.com/article/2013/12/09/super-mario-3d-world-review/"
+            ],
+            "letterboxd.com": [
+              "https://letterboxd.com/film/the-super-mario-bros-movie/",
+              "https://letterboxd.com/justindecloux/film/the-super-mario-bros-movie/",
+              "https://letterboxd.com/film/super-mario-bros/reviews/"
+            ],
+            "www.gamesradar.com": [
+              "https://www.gamesradar.com/new-super-mario-bros-movie-trailer-sees-mario-and-princess-peach-take-on-bowser/",
+              "https://www.gamesradar.com/new-super-mario-bros-2-review/",
+              "https://www.gamesradar.com/the-last-of-us-highest-rated-video-game-adaptation/"
+            ],
+            "collider.com": [
+              "https://collider.com/box-office-hit-movies-bad-reviews/",
+              "https://collider.com/super-mario-bros-movie-thursday-box-office/",
+              "https://collider.com/notable-divides-between-audience-critic-scores-on-rotten-tomatoes/",
+              "https://collider.com/the-super-mario-bros-movie-review/"
+            ],
+            "www.pluggedin.com": [
+              "https://www.pluggedin.com/blog/movie-monday-4-17-23/",
+              "https://www.pluggedin.com/blog/movie-monday-4-10-23/",
+              "https://www.pluggedin.com/blog/movie-monday-5-1-23/",
+              "https://www.pluggedin.com/blog/the-plugged-in-show-episode-177/",
+              "https://www.pluggedin.com/blog/movie-monday-5-15-23/",
+              "https://www.pluggedin.com/blog/movie-monday-4-24-23/",
+              "https://www.pluggedin.com/blog/movie-tuesday-5-30-23/"
+            ],
+            "www.empireonline.com": [
+              "https://www.empireonline.com/movies/reviews/the-super-mario-bros-movie/",
+              "https://www.empireonline.com/movies/reviews/tetris/",
+              "https://www.empireonline.com/movies/news/super-mario-bros-movie-gets-comic-book-sequel/"
+            ]
+          }
+
+        #   ew.com: !ruby/object:Product
+        #     name:
+        #     score:
+        #     maxscore:
+        #     source:
+        #     initial_source: https://ew.com/movies/super-mario-bros-tops-box-office/
+        #     www.pluggedin.com: !ruby/object:Product
+        #     name:
+        #     score:
+        #     maxscore:
+        #     source:
+        #     initial_source: https://www.pluggedin.com/blog/movie-monday-4-17-23/
+        #     collider.com: !ruby/object:Product
+        #     name:
+        #     score:
+        #     maxscore:
+        #     source:
+        #     initial_source: https://collider.com/box-office-hit-movies-bad-reviews/
+        #     www.ign.com: !ruby/object:Product
+        #     name: The Super Mario Bros. Movie Review - IGN
+        #     score: 8.0
+        #     maxscore: 10
+        #     source: https://www.ign.com/articles/the-super-mario-bros-movie-review
+        #     initial_source: https://www.ign.com/articles/the-super-mario-bros-movie-review
+        #     www.rottentomatoes.com: !ruby/object:Product
+        #     name: The Super Mario Bros. Movie
+        #     score: 58
+        #     maxscore: 100
+        #     source: https://www.rottentomatoes.com/m/the_super_mario_bros_movie
+        #     initial_source: https://www.rottentomatoes.com/m/the_super_mario_bros_movie
+        #     www.rogerebert.com: !ruby/object:Product
+        #     name: The Super Mario Bros. Movie
+        #     score: 1.5
+        #     maxscore: 4
+        #     source: https://www.rogerebert.com/reviews/the-super-mario-bros-movie-movie-review-2023
+        #     initial_source: https://www.rogerebert.com/reviews/the-super-mario-bros-movie-movie-review-2023
+        #     www.commonsensemedia.org: !ruby/object:Product
+        #     name: The Super Mario Bros. Movie
+        #     score: 3
+        #     maxscore: 5
+        #     source: https://www.commonsensemedia.org/movie-reviews/the-super-mario-bros-movie
+        #     initial_source: https://www.commonsensemedia.org/movie-reviews/the-super-mario-bros-movie
+        #     www.empireonline.com: !ruby/object:Product
+        #     name: this movie
+        #     score: 2.0
+        #     maxscore: 5
+        #     source: https://www.empireonline.com/movies/reviews/the-super-mario-bros-movie/
+        #     initial_source: https://www.empireonline.com/movies/reviews/the-super-mario-bros-movie/
+        #     www.imdb.com: !ruby/object:Product
+        #     name: The Super Mario Bros. Movie
+        #     score: 7.1
+        #     maxscore: 10
+        #     source: https://www.imdb.com/title/tt6718170/
+        #     initial_source: https://www.imdb.com/title/tt6718170/
+        #     letterboxd.com: !ruby/object:Product
+        #     name:
+        #     score:
+        #     maxscore: 5
+        #     source: https://letterboxd.com/film/super-mario-bros/reviews/
+        #     initial_source: https://letterboxd.com/film/the-super-mario-bros-movie/
+        #     www.metacritic.com: !ruby/object:Product
+        #     name: The super mario bros. movie 2023
+        #     score: 8.4
+        #     maxscore: 10
+        #     source: https://www.metacritic.com/movie/the-super-mario-bros-movie
+        #     initial_source: https://www.metacritic.com/movie/the-super-mario-bros-movie
+        #     www.gamesradar.com: !ruby/object:Product
+        #     name:
+        #     score:
+        #     maxscore: 5
+        #     source: https://www.gamesradar.com/the-last-of-us-highest-rated-video-game-adaptation/
+        #     initial_source: https://www.gamesradar.com/new-super-mario-bros-movie-trailer-sees-mario-and-princess-peach-take-on-bowser/
+
         products_map = @object.get_products(hostname_to_urls, "movies")
         expected_product = Product.new(name: "Dune", score: 83, maxscore: 100, source: "https://www.rottentomatoes.com/m/dune_2021", initial_source: "https://www.rottentomatoes.com/m/dune_2021")
         assert_equal(expected_product, products_map["www.rottentomatoes.com"], "scraped product did not match")
