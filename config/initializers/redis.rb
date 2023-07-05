4ce25a96720e6f955b28846281dba0d41a743a62
Fabian <ornelasfdev@gmail.com>
2023-07-04 20:35:53 -0500

    Added redis exception catch

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,8 +4,13 @@ disable_cache = ENV.fetch("DISABLE_CACHE") { false }
 
 if ENV.fetch("RAILS_ENV") != "test" and not disable_cache
     # The constant below will represent ONE connection, present globally in models, controllers, views etc for the instance. No need to do Redis.new everytime
-    REDIS = Redis.new(host: redis_host, port: redis_port.to_i)
-    puts "Initialized Redis instance. Connected: #{REDIS.info.present?}"
+    begin
+        REDIS = Redis.new(host: redis_host, port: redis_port.to_i)
+        puts "Initialized Redis instance. Connected: #{REDIS.info.present?}"
+    rescue => exception
+        puts "Failed to connect to Redis instance: #{exception.message}"
+        REDIS = nil
+    end
 else
     REDIS = nil
 end
